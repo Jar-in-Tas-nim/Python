fcc4e2a50d6027d198d14dfda3ecdb120365b5f2
Jarin Tasnim <personaljarin@gmail.com>
2024-10-20 14:41:47 +0600

    Add files via upload

---
+++ Project Band Name Generator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator")
+city=input("Which City did you grew up in?\n")
+print(city)
+
+pet=input("What is the name of your school?\n")
+print(pet)
+
+print("Your band name could be "+city+" "+pet)
